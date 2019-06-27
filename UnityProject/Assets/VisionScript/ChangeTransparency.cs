ab35abdebd4193da592dd35c79a864a2177144b5
ivan-victoria <45040627+ivan-victoria@users.noreply.github.com>
2019-06-27 11:08:37 +0200

    Update

--- UnityProject/Assets/VisionScript/ChangeTransparency.cs
+++ UnityProject/Assets/VisionScript/ChangeTransparency.cs
@@ -2,6 +2,8 @@
 // Copyright 2016 Tobii AB (publ). All rights reserved.
 //-----------------------------------------------------------------------
 
+using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 using Tobii.Gaming;
 
@@ -14,18 +16,27 @@ using Tobii.Gaming;
 /// </remarks>
 [RequireComponent(typeof(GazeAware))]
 [RequireComponent(typeof(MeshRenderer))]
+
 public class ChangeTransparency : MonoBehaviour
 {
 
     public Color selectionColor;
-
+    public GameObject text;
     private GazeAware _gazeAwareComponent;
+    private UserPresence _userPresence;
     private MeshRenderer _meshRenderer;
-
+    // public Object texta;
+    public Color textcolor;
     private Color _deselectionColor;
+    // private Color _deselectionColorText;
     private Color _lerpColor;
     private float _fadeSpeed = 0.1f;
 
+    public bool enableHide;
+    public bool enableDesktopHide;
+    private int counter = 0;
+
+    TextMesh textmesh;
     /// <summary>
     /// Set the lerp color
     /// </summary>
@@ -35,6 +46,9 @@ public class ChangeTransparency : MonoBehaviour
         _meshRenderer = GetComponent<MeshRenderer>();
         _lerpColor = _meshRenderer.material.color;
         _deselectionColor = new Color(1f, 1f, 1f, 0.2f);
+        _userPresence = TobiiAPI.GetUserPresence();
+        //  _deselectionColorText = new Color(1f, 1f, 1f, 0.4f);
+        //  textcolor = new Color(0f,0f,0f,1f);
     }
 
     /// <summary>
@@ -46,16 +60,65 @@ public class ChangeTransparency : MonoBehaviour
         if (_meshRenderer.material.color != _lerpColor)
         {
             _meshRenderer.material.color = Color.Lerp(_meshRenderer.material.color, _lerpColor, _fadeSpeed);
+            text.GetComponent<TextMesh>().color = Color.Lerp(_meshRenderer.material.color, textcolor, _fadeSpeed);
+            //  Color.black;
+
         }
 
         // Change the color of the cube
         if (_gazeAwareComponent.HasGazeFocus)
         {
             SetLerpColor(selectionColor);
+            text.GetComponent<TextMesh>().color = textcolor;
+
         }
         else
         {
             SetLerpColor(_deselectionColor);
+        }
+
+        if (enableHide) {
+
+            if (_gazeAwareComponent.HasGazeFocus)
+            {
+                counter++;
+            }
+
+            else
+            {
+                if (counter > 0)
+                {
+                    _meshRenderer.enabled = false;
+                    text.SetActive(false);
+                }
+            }
+
+            if (text.activeSelf == false)
+            {
+                StartCoroutine(updateCoroutine(5, true));
+                counter = 0;
+            }
+
+        }
+
+
+        if (enableDesktopHide)
+        {
+            _userPresence = TobiiAPI.GetUserPresence();
+            if (_userPresence.IsUserPresent())
+            {
+             //   print("A user is present in front of the screen.");
+                if (text.activeSelf == false)
+                {
+                    StartCoroutine(updateCoroutine(1, true));
+                }
+            }
+            else
+            {
+              //  print("User presence status is: " + _userPresence);
+                StartCoroutine(updateCoroutine(5, false));
+            }
+            
         }
     }
 
@@ -67,4 +130,12 @@ public class ChangeTransparency : MonoBehaviour
     {
         this._lerpColor = lerpColor;
     }
+
+    private IEnumerator updateCoroutine(int timer, bool enabler )
+    {
+        yield return new WaitForSecondsRealtime(timer);
+        _meshRenderer.enabled = enabler;
+        text.SetActive(enabler);
+    }
+
 }
